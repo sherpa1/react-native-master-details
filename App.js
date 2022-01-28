42414a6ea2d27472c7d2536f12699e14addd8280
Sherpa <alex@sherpa.one>
2022-01-28 09:49:16 +0100

    alert

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import {
+  ActivityIndicator,
   Button,
   FlatList,
   Pressable,
@@ -12,22 +13,29 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors
-} from 'react-native/Libraries/NewAppScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import axios from 'axios';
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+const UserPreview = ({ item }, navigation) => {
 
+  const on_press = () => {
+    navigation.navigate('Details', { user: item })
+  };
+
+  return (
+    <Pressable style={styles.UserPreview} onPress={() => on_press()}>
+      <Text style={{ color: 'white', fontSize: 30 }}>{item.name}</Text>
+    </Pressable>
+  );
+
+}
+
+const MasterScreen = ({ navigation }) => {
 
   const [users, setUsers] = useState([]);
   const [loadingStatus, setLoadingStatus] = useState(0);
-
-  const UserPreview = ({ item }) => (
-    <Text>{item.name}</Text>
-  );
 
   useEffect(() => {
 
@@ -51,26 +59,86 @@ const App = () => {
 
   }, []);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  return (
+    <SafeAreaView>
+      {loadingStatus === 1 ? <ActivityIndicator /> : null}
+      {(users.length && loadingStatus === 1 || loadingStatus === 0) ?
+        <FlatList data={users} keyExtractor={(item) => item.id} renderItem={(item) => UserPreview(item, navigation)} />
+        : <Text>No users found.</Text>}
+    </SafeAreaView>);
+};
+
+const DetailsScreen = ({ navigation, route }) => {
+
+  const on_press = () => {
+    alert("Do you really want to delete this user ?");
   };
 
   return (
-    <View style={[styles.center, backgroundStyle]}>
-      {loadingStatus === 1 ? <Text>Loading...</Text> : null}
-      {(users.length && loadingStatus === 1 || loadingStatus === 0) ?
-        <FlatList data={users} keyExtractor={(item) => item.id} renderItem={UserPreview} />
-        : <Text>No users found.</Text>}
+    <View style={styles.Screen}>
+      <Text style={styles.text.h1}>{route.params.user.name}</Text>
+      <Text>{route.params.user.username}</Text>
+      <Text>{route.params.user.email}</Text>
+      <Text>{route.params.user.phone}</Text>
+      <Text>{route.params.user.website}</Text>
+      <Text style={styles.text.h2}>Address :</Text>
+      <Text>{route.params.user.address.street}</Text>
+      <Text>{route.params.user.address.suite}</Text>
+      <Text>{route.params.user.address.city}</Text>
+      <Text>{route.params.user.address.zipcode}</Text>
+      <Text>{route.params.user.address.geo.lat}</Text>
+      <Text>{route.params.user.address.geo.lng}</Text>
+      <Text style={styles.text.h2}>Company :</Text>
+      <Text>{route.params.user.company.name}</Text>
+      <Text>{route.params.user.company.bs}</Text>
+      <Button title="Delete" color="red" onPress={() => on_press()} />
     </View>
   );
 };
 
+const App = () => {
+
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Master"
+          component={MasterScreen}
+        />
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen} />
+      </Stack.Navigator>
+
+    </NavigationContainer>
+  );
+};
+
 const styles = StyleSheet.create({
+  text: {
+    h1: { fontSize: 30 },
+    h2: { fontSize: 20 },
+  },
   center: {
     alignItems: 'center',
     justifyContent: 'center',
     flex: 1,
-  }
+  },
+  Screen: {
+    padding: 30,
+    justifyContent: 'space-evenly',
+    flex: 1,
+  },
+  UserPreview: {
+    padding: 10,
+    borderRadius: 4,
+    backgroundColor: "grey",
+    margin: 10,
+  },
+
 });
 
 export default App;
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# React Native Master Details
+# React Native _Master / Details_
 
-Application mobile cross-platforms développée avec React Native illustrant le concept Master / Details
+Application mobile cross-platforms développée avec React Native illustrant le concept ergonomique _Master / Details_.
 
 ## Commandes utiles
 
@@ -9,10 +9,67 @@ Application mobile cross-platforms développée avec React Native illustrant le 
 `npm install`
 
 - (Dans un 1er onglet du terminal) Démarrage du Bundler JS _Metro_ :
+
 `npx react-native start`
 
 - (Dans un 2nd onglet du terminal) Démarrage de l'application sur l'émulateur mobile Android :
+
 `npx react-native run-android`
+
+## Dépendances
+
+https://reactnative.dev/docs/navigation
+
+`npm install @react-navigation/native @react-navigation/native-stack`
+
+`npm install react-native-screens react-native-safe-area-context`
+
+## Documentation
+
+- Activy Indicator : https://reactnative.dev/docs/activityindicator
+
+- Navigation : https://reactnative.dev/docs/navigation
+
+- Flat List : https://reactnative.dev/docs/flatlist
+
+- Confirm Dialog : https://www.kindacode.com/article/how-to-create-a-confirm-dialog-in-react-native/
+
+## API REST
+
+https://jsonplaceholder.typicode.com/users
+
+## Modèle de données __User__ + données imbriquées
+
+- User
+  - Address
+    - Geo
+    - Company
+
+```
+{
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  },
+```
 
 ---
 
